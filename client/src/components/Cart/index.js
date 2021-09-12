c337b35da84d17e38fcdf175b60caca8e856b91f
Liz A <lizethariasc14@gmail.com>
2021-09-12 13:26:51 -0700

    finish indexedDB functionality

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,67 +1,78 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
+
 import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART } from '../../utils/actions';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+import { idbPromise } from '../../utils/helpers';
 
 const Cart = () => {
-const [state, dispatch] = useStoreContext();
-console.log(state)
+    const [state, dispatch] = useStoreContext();
 
-function toggleCart() {
-  dispatch({ type: TOGGLE_CART });
-}
-
-function calculateTotal() {
-    let sum = 0;
-    state.cart.forEach(item => {
-      sum += item.price * item.purchaseQuantity;
-    });
-    return sum.toFixed(2);
-  }
-
-if (!state.cartOpen) {
-    return (
-      <div className="cart-closed" onClick={toggleCart}>
-        <span
-          role="img"
-          aria-label="trash">🛒</span>
-      </div>
-    );
-  }
-
-  return (
-    <div className="cart">
-  <div className="close" onClick={toggleCart}>[close]</div>
-  <h2>Shopping Cart</h2>
-  {state.cart.length ? (
-    <div>
-      {state.cart.map(item => (
-        <CartItem key={item._id} item={item} />
-      ))}
-      <div className="flex-row space-between">
-        <strong>Total: ${calculateTotal()}</strong>
-        {
-          Auth.loggedIn() ?
-            <button>
-              Checkout
-            </button>
-            :
-            <span>(log in to check out)</span>
+    useEffect(() => {
+        // async function to get data from IndexedDB
+        async function getCart() {
+            const cart = await idbPromise('cart', 'get');
+            dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+        };
+        // check global state for any cart products, and if not, use function to retrieve data from the IndexedDB store
+        if (!state.cart.length) {
+            getCart();
         }
-      </div>
-    </div>
-  ) : (
-    <h3>
-      <span role="img" aria-label="shocked">
-        😱
-      </span>
-      You haven't added anything to your cart yet!
-    </h3>
-  )}
-</div>
-  );
+    }, [state.cart.length, dispatch]);
+
+    function toggleCart() {
+        dispatch({ type: TOGGLE_CART });
+    }
+
+    if (!state.cartOpen) {
+        return (
+            <div className="cart-closed" onClick={toggleCart}>
+                <span role="img" aria-label="cart">🛒</span>
+            </div>
+        )
+    }
+
+    function calculateTotal() {
+        let sum = 0;
+        state.cart.forEach(item => {
+            sum += item.price * item.purchaseQuantity;
+        });
+        return sum.toFixed(2);
+    }
+
+    return (
+        <div className="cart">
+            <div className="close" onClick={toggleCart}>[close]</div>
+            <h2>Shopping Cart</h2>
+            {state.cart.length ? (
+                <div>
+                {state.cart.map(item => (
+                    <CartItem key={item._id} item={item} />
+                ))}
+                <div className="flex-row space-between">
+                    <strong>Total: ${calculateTotal()}</strong>
+                    {
+                    Auth.loggedIn() ?
+                        <button>
+                        Checkout
+                        </button>
+                        :
+                        <span>(log in to check out)</span>
+                    }
+                </div>
+                </div>
+            ) : (
+                <h3>
+                <span role="img" aria-label="shocked">
+                    😱
+                </span>
+                You haven't added anything to your cart yet!
+                </h3>
+            )}
+        </div>
+    );
 };
 
 export default Cart;
